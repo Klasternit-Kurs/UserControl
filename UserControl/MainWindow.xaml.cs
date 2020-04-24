93f749d89af9f9cbc9708edb1ed396e3c31b0c9e
Dragan Pepic <d.pepic@hotmail.com>
2020-04-24 18:15:03 +0200

    Add project files.

---
+++ UserControl/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControldsa
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public Kontrola UC = new Kontrola();
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			UC.Labela = "Nesto trece:";
+			sp.Children.Add(UC);
+		}
+
+		private void Dohvati(object sender, RoutedEventArgs e)
+		{
+			MessageBox.Show(UC.Bla);
+		}
+	}
+}
